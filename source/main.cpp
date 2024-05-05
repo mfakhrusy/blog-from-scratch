d3351cd94a066134e5de5ff0f807bbbb45e352e8
Muhamad Fakhrusy (Fahru) <mfakhrusy3@gatech.edu>
2024-05-05 16:55:43 -0700

    Initialize simple C++ HTTP server using socket

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <cstdlib>
+#include <unistd.h> // Include the header file for the close function
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int serverSocket;
+    struct addrinfo hints, *servinfo;
+
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    // Get the address information of the server
+    getaddrinfo("localhost", "8080", &hints, &servinfo);
+
+    serverSocket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    bind(serverSocket, servinfo->ai_addr, servinfo->ai_addrlen);
+
+    // Allow the socket to be reused immediately after the server is closed
+    int yes = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+
+    listen(serverSocket, 10);
+
+    int clientSocket;
+    struct sockaddr_storage clientAddr;
+    socklen_t clientAddrSize = sizeof(clientAddr);
+
+    while(true) {
+        clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+        send(clientSocket, "Hello, World!", 13, 0);
+        close(clientSocket);
+    }
+    
     return 0;
 }
